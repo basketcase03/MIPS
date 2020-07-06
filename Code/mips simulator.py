2a143f55660d3b9d1acbcae53a988f4817d20138
Muskan Khajuria <47385642+basketcase03@users.noreply.github.com>
2020-07-06 23:33:27 +0530

    updated .py file to get correct name

--- Code/mips simulator.py
+++ Code/mips simulator.py
@@ -5,7 +5,7 @@ def get_mips_cache():
     subprocess.run(["Files/mips cache simulator.exe"])
 
 def get_mips_pipeline():
-    subprocess.run(["Files/mips pipeline simulator.exe"])
+    subprocess.run(["Files/mips_pipeline_simulator.exe"])
 
 HEIGHT = 700
 WIDTH = 1040
